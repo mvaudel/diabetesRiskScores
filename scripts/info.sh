f50d45dab8c7576ab7a19ef636cfa948cd3cc109
mvaudel <mvaudel@gmail.com>
2019-01-31 21:05:19 +0100

    Added a script to make the info files.

---
+++ scripts/info.sh
@@ -0,0 +1,2 @@
+
+parallel --jobs 22 --eta echo java -cp bin/drs-0.0.1/drs-0.0.1.jar no.uib.drs.cmd.InfoFile -g /mnt/archive/HARVEST/genotypes-base/imputed/all/{}.vcf.gz -o /mnt/archive/marc/scores/info/moba/harvest/{} -t TYPED -f 0 -s INFO ::: 1..22
